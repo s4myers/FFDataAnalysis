0f92b550c2c3b45a9e3f8ac1588b68ccf71be823
s4myers <sean.myers.01@gmail.com>
2015-07-03 13:52:29 -0700

    added ability to determines averages and std

--- scripts/Data_Analysis.py
+++ scripts/Data_Analysis.py
@@ -1,9 +1,21 @@
 import csv
 import os
+import numpy as np
 
 main_dir = "c:/Users/sean/Documents/GitHub/FFDataAnalysis/CSV_data"  #Change depending on save location
 team_list =["BUF","MIA","NE","NYJ","BAL","CIN","CLE","PIT","HOU","IND","JAC","TEN","DEN","KC","OAK","SD","DAL","NYG","PHI","WAS","CHI","DET","GB","MIN","ATL","CAR","NO","TB","ARI","STL","SF","SEA"]
-year_list = ["2010","2011","2012","2013","2014"]
+year_list = ["2010","2011","2012","2013","2014"]  #Change once if we use more data
+
+#Fantasy point multipliers
+rush_td = 6.0
+rec_td = 6.0
+pass_td = 4.0
+rush_yard = .1
+rec_yard=.1
+pass_yard = 0.04
+ppr = 1.0
+turnover = -2.0
+sack = -1.0
 
 class Player(object):
 	"""An active roster player to research for your fantasy lineup.
@@ -29,6 +41,17 @@ class Player(object):
 				for row in reader:
 					if row["Player"]== self.name:
 						self.stats[row["Year"]][row["WK"]]=row
+
+	def generate_array_stats(self,field,year):
+		temp_list=[]
+		for week in self.stats[year].keys():
+			if self.stats[year][week]["G"]=="0" or self.stats[year][week]["Game Date"]=="Bye":
+				continue
+			elif self.stats["2014"][week][field]=="--":
+				temp_list+=[0.0]
+			else:
+				temp_list+=[float(self.stats["2014"][week][field])]
+		return np.array(temp_list)
 
 
 	def total(self,field,year,weeks=["all_weeks"]):
@@ -65,6 +88,30 @@ class Player(object):
 		except KeyError:
 			print "No data available for this year or invalid weeks"		
 			return
+
+	def average(self,field,year):
+		"""
+		Returns the weekly average of a stat for the the season
+
+		Keyword Arguments:
+		field - The field to total as a string
+		year - the year of interest as a string
+
+		"""
+		ary = self.generate_array_stats(field,year)
+		return np.average(ary)
+
+	def standard_dev(self,field,year):
+		"""
+		Returns the weekly the standard deviation of a stat for the season
+
+		Keyword Arguments:
+		field - The field to total as a string
+		year - the year of interest as a string
+		
+		"""
+		ary = self.generate_array_stats(field,year)
+		return np.std(ary)	
 
 
 class QuarterBack(Player):
