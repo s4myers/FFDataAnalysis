61f49f31ba8e81e51d1ebe7c9110a50c692628fc
justinmcmillan <justin.m.mcmillan@gmail.com>
2016-09-12 22:21:55 -0700

    Added weekly scraping function

--- scripts/PullStats.py
+++ scripts/PullStats.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup, SoupStrainer
 import urllib2 as ul
 from string import printable
 import re, os
+import csv
+import time
 import fileinput
 
 abbr = {'quarterback': 'QB', 'runningback': 'RB', 'widereceiver': 'WR', 'tightend': 'TE', 'kicker': 'K'}
@@ -21,15 +23,16 @@ titlesPulled = False
 def main():
     positions = ["quarterback", "runningback", "widereceiver", "tightend", "kicker"]
     #scrape_current_players(positions)
-    rem_dups()
+    #remove_duplicates()
+    pull_game_stats_weekly(2016, 1)
 
-def rem_dups():
+
+def remove_duplicates():
 
     with open('C:\Users\justin\Documents\GitHub\FFDataAnalysis\CSV_data\CombinedPlayerList.csv','r') as in_file, open('C:\Users\justin\Documents\GitHub\FFDataAnalysis\CSV_data\CombinedPlayerList2.csv','w') as out_file:
         seen = set() # set for fast O(1) amortized lookup
         for line in in_file:
             if line in seen: continue # skip duplicate
-
             seen.add(line)
             out_file.write(line)
 
@@ -65,86 +68,115 @@ def scrape_current_players(positions):
                     print e.reason
                     return False
 
-def pull_game_stats2(url, player_name, firstYear, lastYear, pos):
+
+def pull_game_stats_weekly(season,week):
     """
-    This function is accessed by the generate_url_list() function.
-    It iterates through Game Log pages, pulls stats, and writes to csv file.
+    This function will pull stats for a given week for each active player in the NFL
 
-    :param url: URL to 'Game Log'
-    :param player_name: player name
-    :param yearTemp: year
-    :param pos:
-    :return: writes to output csv files
+    :param season: year value for the given week
+    :param week: week number for the given week
+    :return: writes to csv file
     """
-    try:
-        readPage = ul.urlopen(url).read()
-        soup = BeautifulSoup(readPage, "html.parser")
-        file_path = "../CSV_data/Temp/" + pos + "Stats.csv"
 
-        # assign field names
-        fieldNames = soup.find("tr", {"class" : "player-table-key"}).findAll("td")
-        numColumns = len(fieldNames)
-        global titlesPulled
-        if (titlesPulled == False):
-            outputLine = pos_fields_dict[pos]
-            #for i in range(len(fieldNames)):
-            #    outputLine = fieldNames[i].text, ", "
-            with open(file_path, "a") as text_file:
-                text_file.writelines(outputLine)
-            text_file.close()
-            titlesPulled = True
+# pull url tags for active players into list
+    url_list = []
+    pos_list = []
+    csv_path = "../CSV_data/ActivePlayerList.csv"
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file,skipinitialspace=True)
+        for row in reader:
+            pos_list.append(row[0])
+            url_list.append(row[2])
 
-        # pull the statistics
-        table = soup.findAll("table", {"class":"data-table1"})
-        regularSeason = table[1]
-        for i in range(len(regularSeason)):
-            body = regularSeason.findAll("tbody")
-        body1 = body[0]
-        rows = body1.findAll("tr")
-        rowsList = []
-        for i in range(len(rows)):
-            if len(rows[i]) > 2:
-                rowsList.append(rows[i])
-        del rowsList[len(rowsList)-1]
+# scrape through all player url tags
+    file_path = "../CSV_data/Weekly/"+str(season)+"/"+str(week)+".csv"
+    for pos,url in zip(pos_list,url_list):
+        try:
+            url += str(season)
+            soup = BeautifulSoup(ul.urlopen(url).read(), "html.parser")
 
-        # write statistics to csv
-        for j in range(len(rowsList)):
-            tempRow = rowsList[j]
-            cells = tempRow.findAll("td")
-            output = "\n"
-            for i in range(numColumns): # for each field, append to output string
-                tempCell = str(cells[i]).lstrip("<td>").rstrip("</td>").replace('\t', "").replace('\r', "").replace('\n', "").replace(" ", "")
-                cell = re.sub('<[^>]+>', '', tempCell)
-                cell = re.sub("[^{}]+".format(printable), "", cell)
-                output = output + cell + ","
-                if (tempCell == 'Bye'):
-                    for i in range(numColumns-2):
-                        output = output + ","
-                    break
-            output = output + player_name + "," + str(yearTemp)
-            with open(file_path, "a") as text_file:
-                text_file.writelines(output)
-            text_file.close()
+            # assign field names
+            player_name = soup.find("span", {"class" : "player-name"}).string
+            print player_name, url
+            fieldNames = soup.find("tr", {"class" : "player-table-key"}).findAll("td")
+            numColumns = len(fieldNames)
+            global titlesPulled
+            if (titlesPulled == False):
+               #outputLine = pos_fields_dict[pos]
+               # for i in range(len(fieldNames)):
+               #    outputLine = fieldNames[i].text, ", "
+               # with open(file_path, "a") as text_file:
+               #     text_file.writelines(outputLine)
+               # text_file.close()
+                titlesPulled = True
 
-    except IOError, e:
-        print 'Failed to open url'
-        print '-------------------------------------'
-        if hasattr(e, 'code'):
-            print 'We failed with error code - %s.' % e.code
-        elif hasattr(e, 'reason'):
-            print "The error object has the following 'reason' attribute :"
-            print e.reason
-            return False
+            # pull the statistics
+            table = soup.findAll("table", {"class":"data-table1"})
+            regularSeason = table[1]
 
-    except IndexError:
-        print 'No regular season data.'
-        return False
+            for i in range(len(regularSeason)):
+                body = regularSeason.findAll("tbody")
+            body1 = body[0]
+            rows = body1.findAll("tr")
+            rowsList = []
+            for i in range(len(rows)):
+                if len(rows[i]) > 2:
+                    rowsList.append(rows[i])
+# remove row[0] which contains field names
+            del rowsList[len(rowsList)-1]
 
-    except AttributeError:
-        print 'No regular season data.'
-        return False
+            # write statistics to csv
+            for j in range(len(rowsList)):
+                tempRow = rowsList[j]
+                cells = tempRow.findAll("td")
+                output = ""
+                if (cells[0].string == str(week)):
+                    for i in range(numColumns): # for each field, append to output string
+                        tempCell = str(cells[i]).lstrip("<td>").rstrip("</td>").replace('\t', "").replace('\r', "").replace('\n', "").replace(" ", "")
+                        #print "tempCell: " + tempCell + " i: " + str(i)
+                        cell = re.sub('<[^>]+>', '', tempCell)
+                        cell = re.sub("[^{}]+".format(printable), "", cell)
+                        output = output + cell + ","
+                        if (tempCell == 'Bye'):
+                            for i in range(numColumns-2):
+                                output = output + ","
+                                print "Bye Week Found"
+                            break
+                if output != "":
+                    output = "\n" + output + player_name.strip() + "," + str(season)
+                    print output
+                    print file_path
+                    file_path = "../CSV_data/Weekly/"+pos+"Stats.csv"
+                    with open(file_path, "a") as text_file:
+                        print "Writing to..." + file_path
+                        text_file.write(output)
+                    text_file.close()
+            time.sleep(.05)
 
-def pull_game_stats(url, player_name, yearTemp, pos):
+
+            print '-------------------------------------'
+        except IOError, e:
+            print 'Failed to open url'
+            print '-------------------------------------'
+            if hasattr(e, 'code'):
+                print 'We failed with error code - %s.' % e.code
+            elif hasattr(e, 'reason'):
+                print "The error object has the following 'reason' attribute :"
+                print e.reason
+                return False
+
+        except IndexError:
+            print 'No regular season data: Index error'
+            print '-------------------------------------'
+            #return False
+
+        except AttributeError:
+            print 'No regular season data: Attribute error'
+            print '-------------------------------------'
+            #return False
+
+
+def pull_game_stats_old(url, player_name, yearTemp, pos):
     """
     This function is accessed by the generate_url_list() function.
     It iterates through Game Log pages, pulls stats, and writes to csv file.
